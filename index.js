3e7a6a9dfb110d87accb5e573d1679cb20959f32
samjanderson <sam.andersonnn@live.ca>
2021-01-15 02:45:02 +0000

    iss 3 complete

--- index.js
+++ index.js
@@ -23,3 +23,37 @@
 
 //   console.log('It worked! Returned coordinates:', coordinates);
 // });
+
+
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
+
+const { fetchISSFlyOverTimes } = require('./iss');
+//at this point getting the function in 79 but its just sitting there
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+//this is where we first call the function, just something being save in memory
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  // console.log(error) here it starts looking over to line 80
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned pass over times:', passTimes);
+});
+
+//it is in memory in line 42, computer already knows its there
+
+//fetchISSFlyOverTimes(a, b)
+
+//so we wrote the function in iss.js but we call it index after requiring it
+//so we have our anonymous callback function as an argument
+//function runs goes through when a condition matches bam it calls the callback
+//the callback needs two paramets so we pass it the two parameters to be able to finish
--- iss.js
+++ iss.js
@@ -52,7 +52,7 @@ const fetchCoordsByIP = function(ip, callback) { //we really dont need an ip her
     // const { latitude, longitude } = JSON.parse(body);
 
     // callback(null, { latitude, longitude });
-    console.log(JSON.parse(body));
+    // console.log(JSON.parse(body));
     callback(null, {
       latitude: JSON.parse(body).latitude,
       longitude: JSON.parse(body).longitude
@@ -61,8 +61,52 @@ const fetchCoordsByIP = function(ip, callback) { //we really dont need an ip her
   });
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+
+//this is the request we need http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON
+//when calling from indexjs the coords should be passed in using the same obj format our previous object returned
+//it should pass back the array of obkects inside the response property
+ 
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+
+  //this is request library callback not our (error, response etc)
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    if (error) { //this condition works
+      return callback(error, null); //this is where we start to use the callback function
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS Fly Over Times. Response: ${body}`), null);
+      return;
+    }
+
+    // console.log("what the hell is going on")
+    // const { latitude, longitude } = JSON.parse(body);
+
+    // callback(null, { latitude, longitude });
+    return callback(null, JSON.parse(body).response);
+    
+
+  });
+};
+
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 };
